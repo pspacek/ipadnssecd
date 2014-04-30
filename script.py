d43ea8f20d2550388b2905ac2369a3ad4c9f6007
Petr Spacek <pspacek@redhat.com>
2014-04-30 19:35:41 +0200

    kinit & GSSAPI auth to LDAP works

--- script.py
+++ script.py
@@ -1,13 +1,19 @@
 from ipalib import api
 from ipapython.dn import DN
+from ipapython import ipautil
 from ipaserver.plugins.ldap2 import ldap2
+
+ccache_dir = '/tmp'
+ccache_fn = '%s/ccache' % ccache_dir
+
+ipautil.kinit_hostprincipal('/etc/named.keytab', ccache_dir, 'DNS/vm-151.idm.lab.eng.brq.redhat.com')
 
 api.bootstrap()
 api.finalize()
 
 ldap = api.Backend[ldap2]
-ldap.connect()
+ldap.connect(ccache=ccache_fn)
 
-#print ldap.get_entry(DN('cn=config'))
+print ldap.get_entry(DN('idnsname=ipa.example,cn=dns,dc=ipa,dc=example'))
 
 print ldap.conn.conn
