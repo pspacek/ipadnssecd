bebc87d146687a7356ae893c6d4b74ee4e0238aa
Petr Spacek <pspacek@redhat.com>
2014-05-16 10:46:24 +0200

    oddeleni spravy slovniku do samostatne tridy

--- zoneparser.py
+++ zoneparser.py
@@ -1,25 +1,48 @@
 #!/usr/bin/python
 
+import logging
 from lxml import etree
+import dns.name
 
 
-class ODSZoneListReader(object):
+class ZoneListReader(object):
+    def __init__(self):
+        self.zone_names = set()  # dns.name
+        self.zone_uuids = set()  # UUID strings
+        self.zones = set()       # tuples (dns.name, UUID)
+
+    def _add_zone(self, name, zid):
+        # detect duplicate zone names
+        name = dns.name.from_text(name)
+        assert name not in self.zone_names, \
+            'duplicate name (%s, %s) vs. %s' % (name, zid, self.zones)
+        # duplicate non-None zid is not allowed
+        assert not zid or zid not in self.zone_uuids, \
+            'duplicate UUID (%s, %s) vs. %s' % (name, zid, self.zones)
+
+        self.zone_names.add(name)
+        self.zone_uuids.add(zid)
+        self.zones.add((name, zid))
+
+
+class ODSZoneListReader(ZoneListReader):
     def __init__(self, zonelist_fn):
-        self.ods_zones = {}
+        super(ODSZoneListReader, self).__init__()
+        self.log = logging.getLogger(__name__)
         # hack: zone object UUID is stored as path to imaginary zone file
         self.entryUUID_prefix = "/var/lib/ipa/dns/zone/entryUUID/"
         self.entryUUID_prefix_len = len(self.entryUUID_prefix)
         xml = etree.parse(open('zonelist.xml', 'r'))
-        self.__parse_zonelist(xml)
+        self._parse_zonelist(xml)
 
-    def __parse_zonelist(self, xml):
+    def _parse_zonelist(self, xml):
         """iterate over Zone elements with attribute 'name' and
-        add IPA zones to self.ods_zones"""
+        add IPA zones to self.zones"""
         for zone_xml in xml.xpath('/ZoneList/Zone[@name]'):
-            name, zid = self.__parse_ipa_zone(zone_xml)
-            self.__add_zone(name, zid)
+            name, zid = self._parse_ipa_zone(zone_xml)
+            self._add_zone(name, zid)
 
-    def __parse_ipa_zone(self, zone_xml):
+    def _parse_ipa_zone(self, zone_xml):
         """Extract zone name, input adapter and detect IPA zones.
 
         IPA zones have contains Adapters/Input/Adapter element with
@@ -39,14 +62,9 @@ class ODSZoneListReader(object):
 
         path = in_adapters[0].text
         # strip prefix from path
-        path = path[self.entryUUID_prefix_len:]
-        return (name, path)
+        zid = path[self.entryUUID_prefix_len:]
+        return (name, zid)
 
-    def __add_zone(self, name, zid):
-        # Duplicate zone name should not ever pop up.
-        assert name not in self.ods_zones
-        self.ods_zones[name] = zid
-
-
+logging.basicConfig(level=logging.DEBUG)
 zl = ODSZoneListReader('/tmp/zonelist.xml')
-print zl.ods_zones
+print zl.zones
