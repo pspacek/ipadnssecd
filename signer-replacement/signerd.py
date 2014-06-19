d54d7115ad929f950507f98be6e03cffe32b1ebb
Petr Spacek <pspacek@redhat.com>
2014-06-19 13:37:31 +0200

    uklada metadata do LDAPu, chybi ale flagy a spravny commonname

--- signer-replacement/signerd.py
+++ signer-replacement/signerd.py
@@ -29,24 +29,26 @@ ODS_SE_MAXLINE = 1024  # from ODS common/config.h
 ODS_DB_PATH = '/var/opendnssec/kasp.db'
 ODS_DB_LOCK_PATH = '/var/opendnssec/kasp.db.our_lock'
 
-def sql2ldap_time(sql_time):
-    dt = datetime.strptime(sql_time, "%Y-%m-%d %H:%M:%S")
-    return "%sZ" % dt.strftime("%Y%m%d%H%M%S")
+def sql2datetime(sql_time):
+    return datetime.strptime(sql_time, "%Y-%m-%d %H:%M:%S")
 
-def sql2ldap_times(row):
-    row2key_map = {'generate': 'Created',
-                   'publish': 'Publish',
-                   'active': 'Activate',
-                   'retire': 'Inactive',
-                   'dead': 'Delete'}
+def datetime2ldap(dt):
+    return "%sZ" % dt.strftime(ipalib.constants.LDAP_GENERALIZED_TIME_FORMAT)
+
+def sql2datetimes(row):
+    row2key_map = {'generate': 'idnsSecKeyCreated',
+                   'publish': 'idnsSecKeyPublish',
+                   'active': 'idnsSecKeyActivate',
+                   'retire': 'idnsSecKeyInactive',
+                   'dead': 'idnsSecKeyDelete'}
     times = {}
     for column, key in row2key_map.iteritems():
         if row[column] is not None:
-            times[key] = sql2ldap_time(row[column])
+            times[key] = sql2datetime(row[column])
     return times
 
 def sql2ldap_algorithm(sql_algorithm):
-    return {"Algorithm": dns.dnssec.algorithm_to_text(sql_algorithm)}
+    return {"idnsSecAlgorithm": dns.dnssec.algorithm_to_text(sql_algorithm)}
 
 def ksmutil(params):
     """Call ods-ksmutil with given parameters and return stdout + stderr.
@@ -110,6 +112,37 @@ def get_ldap_keys(ldap, zone_dn):
 
     return ldap_keys
 
+def get_ods_keys(zone_name):
+    # Open DB directly and read key timestamps etc.
+    with ods_db_lock():
+        db = sqlite3.connect(ODS_DB_PATH, isolation_level="EXCLUSIVE")
+        db.row_factory = sqlite3.Row
+        db.execute('BEGIN')
+
+        # get zone ID
+        cur = db.execute("SELECT id FROM zones WHERE LOWER(name)=LOWER(?)",
+                         (zone_name,))
+        rows = cur.fetchall()
+        assert len(rows) == 1, "exactly one DNS zone should exist in ODS DB"
+        zone_id = rows[0][0]
+
+        # get all keys for given zone ID
+        cur = db.execute("SELECT kp.HSMkey_id, kp.generate, kp.algorithm, dnsk.publish, dnsk.active, dnsk.retire, dnsk.dead, dnsk.keytype "
+                 "FROM keypairs AS kp JOIN dnsseckeys AS dnsk ON kp.id = dnsk.id "
+                 "WHERE dnsk.zone_id = ?", (zone_id,))
+        keys = {}
+        for row in cur:
+            key_data = sql2datetimes(row)
+            if 'idnsSecKeyDelete' in key_data \
+                and key_data['idnsSecKeyDelete'] > datetime.now():
+                    continue  # ignore deleted keys
+
+            key_data.update(sql2ldap_algorithm(row['algorithm']))
+            key_id = "%s" % (row['HSMkey_id'])
+            keys[key_id] = key_data
+
+        return keys
+
 
 log = logging.getLogger('root')
 log.addHandler(systemd.journal.JournalHandler())
@@ -144,73 +177,81 @@ zone_name = cmd[7:].strip()
 if len(zone_name) > 1 and zone_name[-1] == '.':
     zone_name = zone_name[:-1]
 
-# Open DB directly and read key timestamps etc.
-with ods_db_lock():
-    db = sqlite3.connect(ODS_DB_PATH, isolation_level="EXCLUSIVE")
-    db.row_factory = sqlite3.Row
-    db.execute('BEGIN')
+ods_keys = get_ods_keys(zone_name)
+ods_keys_id = set(ods_keys.keys())
 
-    # get zone ID
-    cur = db.execute("SELECT id FROM zones WHERE LOWER(name)=LOWER(?)", (zone_name,))
-    rows = cur.fetchall()
-    assert len(rows) == 1, "exactly one DNS zone should exist in ODS DB"
-    zone_id = rows[0][0]
+# connect to LDAP
 
-    # get all keys for given zone ID
-    cur = db.execute("SELECT kp.HSMkey_id, kp.generate, kp.algorithm, dnsk.publish, dnsk.active, dnsk.retire, dnsk.dead, dnsk.keytype "
-             "FROM keypairs AS kp JOIN dnsseckeys AS dnsk ON kp.id = dnsk.id "
-             "WHERE dnsk.zone_id = ?", (zone_id,))
+# IPA framework initialization
+ipalib.api.bootstrap()
+ipalib.api.finalize()
 
+# Kerberos initialization
+PRINCIPAL = str('%s/%s' % (DAEMONNAME, ipalib.api.env.host))
+log.debug('Kerberos principal: %s', PRINCIPAL)
+ipautil.kinit_hostprincipal(KEYTAB_FB, WORKDIR, PRINCIPAL)
 
+# LDAP initialization
+dns_dn = DN(ipalib.api.env.container_dns, ipalib.api.env.basedn)
 
+ldap = ipalib.api.Backend[ldap2]
+# fixme
+ldap.connect(ccache="%s/ccache" % WORKDIR)
 
+ldap_zone = get_ldap_zone(ldap, dns_dn, zone_name)
+zone_dn = ldap_zone.dn
 
-
-    # connect to LDAP
-
-    # IPA framework initialization
-    ipalib.api.bootstrap()
-    ipalib.api.finalize()
-
-    # Kerberos initialization
-    PRINCIPAL = str('%s/%s' % (DAEMONNAME, ipalib.api.env.host))
-    log.debug('Kerberos principal: %s', PRINCIPAL)
-    ipautil.kinit_hostprincipal(KEYTAB_FB, WORKDIR, PRINCIPAL)
-
-    # LDAP initialization
-    dns_dn = DN(ipalib.api.env.container_dns, ipalib.api.env.basedn)
-
-    ldap = ipalib.api.Backend[ldap2]
-    # fixme
-    ldap.connect(ccache="%s/ccache" % WORKDIR)
-
-    ldap_zone = get_ldap_zone(ldap, dns_dn, zone_name)
-    zone_dn = ldap_zone.dn
-
+keys_dn = get_ldap_keys_dn(zone_dn)
+try:
+    ldap_keys = get_ldap_keys(ldap, zone_dn)
+except ipalib.errors.NotFound:
+    # cn=keys container does not exist, create it
+    ldap_keys = []
+    ldap_keys_container = ldap.make_entry(keys_dn,
+                                          objectClass=['nsContainer'])
     try:
-        ldap_keys = get_ldap_keys(ldap, zone_dn)
-    except ipalib.errors.NotFound:
-        # cn=keys container does not exist, create it
-        ldap_keys = []
-        keys_dn = get_ldap_keys_dn(zone_dn)
-        ldap_keys_container = ldap.make_entry(keys_dn,
-                                              objectClass=['nsContainer'])
-        try:
-            ldap.add_entry(ldap_keys_container)
-        except ipalib.errors.DuplicateEntry:
-            # ldap.get_entries() do not distinguish non-existent base DN
-            # from empty result set so addition can fail because container
-            # itself exists already
-            pass
+        ldap.add_entry(ldap_keys_container)
+    except ipalib.errors.DuplicateEntry:
+        # ldap.get_entries() do not distinguish non-existent base DN
+        # from empty result set so addition can fail because container
+        # itself exists already
+        pass
 
+ldap_keys_dict = {}
+for ldap_key in ldap_keys:
+    cn = ldap_key['cn'][0]
+    ldap_keys_dict[cn] = ldap_key
 
-    print ldap_keys
+ldap_keys = ldap_keys_dict  # shorthand
+ldap_keys_id = set(ldap_keys.keys())
 
-    sys.exit(0)
+new_keys_id = ods_keys_id - ldap_keys_id
+log.info('new keys from ODS: %s', new_keys_id)
+for key_id in new_keys_id:
+    cn = "cn=%s" % key_id
+    key_dn = DN(cn, keys_dn)
+    log.debug('adding key "%s" to LDAP', key_dn)
+    ldap_key = ldap.make_entry(key_dn,
+                               objectClass=['idnsSecKey'], idnsSecKeyRef='cn=FIXME', **ods_keys[key_id])
+    ldap.add_entry(ldap_key)
 
-    for row in cur:
-        ldap = {}
-        ldap.update(sql2ldap_times(row))
-        ldap.update(sql2ldap_algorithm(row['algorithm']))
-        print ldap
-    log.info('Update terminated !!!')
+deleted_keys_id = ldap_keys_id - ods_keys_id
+log.info('deleted keys in LDAP: %s', deleted_keys_id)
+for key_id in deleted_keys_id:
+    cn = "cn=%s" % key_id
+    key_dn = DN(cn, keys_dn)
+    log.debug('deleting key "%s" from LDAP', key_dn)
+    ldap.delete_entry(key_dn)
+
+update_keys_id = ldap_keys_id.intersection(ods_keys_id)
+log.info('keys in LDAP & ODS: %s', update_keys_id)
+for key_id in update_keys_id:
+    ldap_key = ldap_keys[key_id]
+    ods_key = ods_keys[key_id]
+    log.debug('updating key "%s" in LDAP', ldap_key.dn)
+    ldap_key.update(ods_key)
+    try:
+        ldap.update_entry(ldap_key)
+    except ipalib.errors.EmptyModlist:
+        continue
+
